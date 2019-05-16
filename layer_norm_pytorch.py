ae0bd7979d6fb57c16366d75e83af702cd1e8b45
sxjscience <sxjscience001@gmail.com>
2019-05-16 17:51:52 +0800

    Update layer_norm_pytorch.py

--- layer_norm_pytorch.py
+++ layer_norm_pytorch.py
@@ -68,7 +68,7 @@ def check_ln_speed(use_apex, nbatch, nchannel, eps, nrepeat):
     th.cuda.synchronize()
     fwd_time = 0
     bwd_time = 0
-    for i in range(nrepeat):
+    for i in range(nrepeat + 1):
         in_data = th.randn(B, C, device=device, dtype=dtype, requires_grad=True)
         ograd = th.randn(B, C, device=device, dtype=dtype)
         npy_in_data = in_data.cpu().detach().numpy()
@@ -82,11 +82,13 @@ def check_ln_speed(use_apex, nbatch, nchannel, eps, nrepeat):
             start = time.time()
             out_data = layer(in_data)
             th.cuda.synchronize()
-            fwd_time += time.time() - start
+            if i > 0:
+                fwd_time += time.time() - start
             start = time.time()
             out_data.backward([ograd])
             th.cuda.synchronize()
-            bwd_time += time.time() - start
+            if i > 0:
+                bwd_time += time.time() - start
         npy_th_out_data = out_data.cpu().detach().numpy()
         npt.assert_allclose(npy_th_out_data, gt_out, 1E-5, 1E-5)
 
